b986ae13ea26592942892d71e7f9e2a59d0e4a7a
Sarterin <sarterion137@gmail.com>
2024-07-05 14:57:45 +0300

    Add routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import mongoose from 'mongoose'
+import router from './router'
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -12,6 +13,7 @@ dotenv.config()
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
+app.use('/api', router)
 
 const start = async () => {
     try {
