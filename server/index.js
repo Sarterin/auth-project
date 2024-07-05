d3af98336bf07461d713f2dbf0707cf80d7fa213
Sarterin <sarterion137@gmail.com>
2024-07-05 12:25:50 +0300

    Connect mongoDB and mongoose

--- server/index.js
+++ server/index.js
@@ -3,13 +3,18 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 5000
-
 const app = express()
+
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
 
 const start = async () => {
     try {
+        await mongoose.connect(process.env.DB_URL)
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
         console.log(e)
