dadbdb3a127fc7b1bb26726c669613ec1333621d
Sarterin <sarterion137@gmail.com>
2024-07-05 13:56:27 +0300

    Add user and token models

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
-require('dotenv').config()
-
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const mongoose = require('mongoose')
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import mongoose from 'mongoose'
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
+dotenv.config()
 
 app.use(cors())
 app.use(cookieParser())
