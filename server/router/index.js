b1e3c4cf298a895d96332680267a9141e19396e1
Sarterin <sarterion137@gmail.com>
2024-07-06 16:50:41 +0300

    Implement form fields validation

--- server/router/index.js
+++ server/router/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import userController from "../controllers/user-controller.js";
+import {body} from "express-validator";
 
 const router= express.Router();
 
-router.post('/registration', userController.registration)
+router.post('/registration',
+    body('email').isEmail(),
+    body('password').isLength({min: 6, max: 16}),
+    userController.registration)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 router.get('/activate/:link', userController.activate)
