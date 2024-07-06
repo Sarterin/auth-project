870831abd4b16a127f2ad4ec4c90a47d5d9fa607
Sarterin <sarterion137@gmail.com>
2024-07-06 18:04:26 +0300

    Implement logout and refresh functions

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import TokenModel from "../models/token-model.js";
+import tokenModel from "../models/token-model.js";
 
 class TokenService {
     generateTokens(payload) {
@@ -8,6 +9,24 @@ class TokenService {
         return {
             accessToken,
             refreshToken
+        }
+    }
+
+    validateAccessToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
+            return userData
+        } catch (e) {
+            return null
+        }
+    }
+
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+            return userData
+        } catch (e) {
+            return null
         }
     }
 
@@ -22,6 +41,16 @@ class TokenService {
         }
         const token = await TokenModel.create({user: userId, refreshToken})
         return token
+    }
+
+    async removeToken(refreshToken) {
+        const tokenData = await tokenModel.deleteOne({refreshToken})
+        return tokenData
+    }
+
+    async findToken(refreshToken) {
+        const tokenData = await tokenModel.findOne({refreshToken})
+        return tokenData
     }
 }
 
